3622ae692359adc3baee57d29ee6315d99bccb6a
cristian.a.espin@gmail.com <cristian@Cristians-MacBook-Pro-2.local>
2018-01-02 16:02:25 +0000

    Authentication and Token

--- routes/users.js
+++ routes/users.js
@@ -46,15 +46,13 @@ router.post('/authenticate', (req, res, next) => {
         User.comparePassword(password, user.password, (err, isMatch) => {
             if (err) throw err;
             if (isMatch) {
-                const token = jwt.sign({
-                    data: user
-                }, config.secret, {
+                const token = jwt.sign(user, config.secret, {
                     expiresIn: 604800 // 1 week
                 });
 
                 res.json({
                     success: true,
-                    token: "JWT" + token,
+                    token: 'JWT ' + token,
                     user: {
                         id: user._id,
                         name: user.name,
